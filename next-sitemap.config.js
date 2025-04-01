eb7b4178c0c4b2d9659caf1fd4b319c40f2c95fd
Graham Thomson <88112869+Blitzalpha16@users.noreply.github.com>
2025-04-01 05:44:29 -0400

    feat: Replace built-in sitemap with next-sitemap package

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://canscanapp.ca',
+  generateRobotsTxt: true, // Optional: Generate a robots.txt file as well
+  // Optional: Add any paths to exclude if needed
+  // exclude: ['/server-sitemap.xml'], 
+  // Optional: Default is 'public', change if your static export outputs elsewhere and you want sitemap there
+  // outDir: './out', 
+};
